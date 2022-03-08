612cb15ed5c6023e7a25c332470d7b371b8ecc4d
yonsina94 <yansimora@gmail.com>
2022-03-07 21:17:26 -0400

    minimal changes applied

--- webApi/superShop_API/Controllers/CategoryController.cs
+++ webApi/superShop_API/Controllers/CategoryController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using superShop_API.Controllers.Base;
 using superShop_API.Database.DTOs;
 using superShop_API.Database.Entities;
@@ -6,6 +7,7 @@ using superShop_API.Database.Services.Constructor;
 
 namespace superShop_API.Controllers;
 
+[AllowAnonymous]
 public class CategoryController : BaseController<CategoryService, CategoryDto, Category>
 {
     public CategoryController(IServiceConstructor _constructor) : base(_constructor)
