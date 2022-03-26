391f340e19fd6a76875243ebf8cdd925f07e0f24
yonsina94 <yansimora@gmail.com>
2022-03-26 18:19:42 -0400

    changes applied

---
+++ webApi/superShop_API/Migrations/20220326221850_orderTranssaciontionDetailsCorrection.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace superShop_API.Migrations
+{
+    public partial class orderTranssaciontionDetailsCorrection : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "transactionDetails",
+                table: "Orders",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "transactionDetails",
+                table: "Orders",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
